86a0330905300cddc2108cace02a94ba33aeee6a
Lans Quintero <lansquintero@gmail.com>
2021-12-12 18:53:29 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, {useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch']) //'Samurai X', 'Dragon Ball']);
+
+    
+
+    //const handledAdd = () => {
+      //  setCategories(['Los Simpsons', ...categories])
+    //}
+
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories={setCategories}/>
+
+            <hr/>
+
+                <ol>
+                {
+                    categories.map( category  => (  
+                        <GifGrid key = {category}
+                                category= {category} 
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
---
+++ src/components/AddCategory.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ({setCategories}) => {
+
+    const [inputValue, setinputValue] = useState('');
+
+    const handleInputChange = (e) => {
+        //console.log(e.target.value)
+        setinputValue(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //console.log('submit hecho')
+
+        if(inputValue.trim().length > 2){
+            setCategories(categ => [inputValue, ...categ]);
+            setinputValue('');
+        }
+        
+    }
+
+    return (
+        <form onSubmit= {handleSubmit}>
+            <input
+                type = "text"
+                value= {inputValue}
+                onChange= {handleInputChange}
+            />
+        </form>
+    )
+}
+
+AddCategory.propTypes= {
+    setCategories: PropTypes.func.isRequired
+}
